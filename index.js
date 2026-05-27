5a58893495cd65edcfe86c5982849733eba0232a
innosoltech <innovativesolgh@gmail.com>
2026-05-27 19:19:44 +0000

    updates made to index.js, index.css & index.html

--- index.js
+++ index.js
@@ -10,4 +10,31 @@ document.addEventListener("DOMContentLoaded", () => {
       video.play();
     });
   });
+
+  $(".owl-carousel").owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 2000,
+    loop: true,
+    margin: 10,
+    nav: true,
+    rewind: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
+
+const navToggle = document.getElementById("navToggle");
+const navLinks = document.getElementById("navLinks");
+
+navToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
